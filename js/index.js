9c7371ffdb9ea4ab22512953529bad558f57d1fa
Tiffany Lam <tlam5@sbcglobal.net>
2017-01-02 17:51:29 -0800

    Gets numbers and operations to display when clicked

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// on click, numbers & operations appended to calculation div via data attr
+
+var buttons = document.querySelectorAll('.btn');
+var calculation = document.getElementById('calculation');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    var btnNum = this.getAttribute("data-num");
+    var btnOp = this.getAttribute("data-operation");
+    if (!btnOp) {
+      calculation.innerText += btnNum;
+    } else if (!btnNum) {
+      calculation.innerText += btnOp;
+    }
+  })
+}
+
+// get btn background color to change when clicked
+
+// get calculation div to clear when AC is clicked
