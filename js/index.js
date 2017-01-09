ed4e0461bfc52c198d55d82649c61e1f7dd42fd2
Tiffany Lam <tlam5@sbcglobal.net>
2017-01-08 23:14:11 -0800

    Evaluates expression and displays result when equals is clicked

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var result = document.getElementById('result');
 var input = [];
 var total;
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var operators = ["x", "÷", "+", "-"];
+var operators = ["*", "/", "+", "-"];
 
 function removeTransition() {
   this.classList.remove('clicked');
@@ -14,6 +14,10 @@ function removeTransition() {
 function update(buttonValue) {
   input.push(buttonValue);
   total = input.join('');
+}
+
+function truncate(string) {
+
 }
 
 for (var i = 0; i < buttons.length; i++) {
@@ -29,12 +33,19 @@ for (var i = 0; i < buttons.length; i++) {
       input = [];
       total = input.join('');
       calculation.textContent = total;
+      result.textContent = total;
     }
     // if CE is clicked
     else if (btnVal === 'ce') {
       input.pop();
       total = input.join('');
       calculation.textContent = total;
+      result.textContent = '';
+    }
+    //if = is clicked
+    else if (btnVal === "=") {
+      total = eval(input.join(''));
+      result.textContent = truncate(total);
     }
     // if a number is clicked
     else if (btnVal !== NaN && numbers.indexOf(parseInt(btnVal)) > -1) {
@@ -56,3 +67,8 @@ for (var i = 0; i < buttons.length; i++) {
   // e.g. +/- 20 is the same as 20 +/-
   // but % 80 doesn't work, 80 % does
 // what if >=2 operation buttons clicked consecutively?
+
+// truncate repeating decimals: toFixed or ?
+// change divide and multiply to / and * when evaluating instead?
+
+// change display css on calculation & result so they're in fixed positions
